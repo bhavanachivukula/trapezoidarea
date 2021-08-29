44c0f274db6d57336aafb3f9380a4c6e7440525a
Bhavana Chivukula <bchivukula1999@gmail.com>
2021-08-29 11:59:24 +0530

    Feature/multiplication was rebased with feature/divisionbytwo branch

--- trap.py
+++ trap.py
@@ -1,11 +1,13 @@
-<<<<<<< HEAD
 def add(a,b):
     return a+b
 
 def divisionbytwo(n):
     return n//2
 
-addValue=add(2,2)
-print(divisionbytwo(addValue))
+def multiplication(divisionValue,h):
+    return divisionValue*h
 
+addValue=add(2,2)
+divisionValue=divisionbytwo(addValue)
+print(multiplication(divisionValue,3))
 
